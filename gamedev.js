bf776ad370ac272610da7e3f9fd070b2d5419224
alienzzz <200852@hkis.edu.hk>
2015-12-17 07:21:19 +0800

    change picture attempt

--- gamedev.js
+++ gamedev.js
@@ -15,13 +15,13 @@ $( document ).ready(function() {
 	});
 
 	$("#goRight-Straight2").click(function() {
-		$("#exploreBody").css("background-image", "url('http://bicycletouringpro.com/blog/wp-content/uploads/2014/04/scary.jpg-588x392.jpg')");
+		$("#exploreBody").css("background-image", "url('http://http://orig10.deviantart.net/1c2c/f/2013/213/a/e/dark_forest_by_narcotic_nightmares-d6g6fgr.png')");
 		$("#goRight-Straight2").hide();
 		$("#goRight-Straight3").show();
 	});
 
 	$("#goRight-Straight3").click(function() {
-		$("#exploreBody").css("background-image", "url('http://bicycletouringpro.com/blog/wp-content/uploads/2014/04/scary.jpg-588x392.jpg')");
+		$("#exploreBody").css("background-image", "url('https://s-media-cache-ak0.pinimg.com/originals/1a/10/20/1a1020c836d312075e3157b5c61c451d.jpg')");
 		$("#goRight-Straight3").hide();
 		$("#goRight-Right2").show();
 	});
